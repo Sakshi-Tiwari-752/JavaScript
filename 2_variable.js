e6ba3b9b53a716d09dddd9c92fc49a20752879d9
Sakshi-Tiwari-752 <121427613+Sakshi-Tiwari-752@users.noreply.github.com>
2023-09-21 08:11:09 +0000

    variable done

---
+++ 2_variable.js
@@ -0,0 +1,33 @@
+"use strict"; //treat all js code as newer version 
+
+//alert statement
+
+// alert("don't do this")  //we are using nodejs, not browser
+
+console.log(3+3);
+
+//tc39.es (it is from javascript) & mdn(is from mozrella) javascript documentation file
+
+
+let name = "sakshi"
+let age = 12;
+let isloggedIn = false
+
+
+//DATATYPE
+ 
+     //primitive
+
+//  number = 2 to power of 53
+//  bigint 
+//  string = " "
+//  boolean =  true/false
+//  null = standalone value  means value are not assined (null is object)
+//  undefined   (it is type of object )
+//  symbol = unique
+
+//  object
+
+//  how to find the datatype 
+
+console.log(typeof age);
